36ac720eb7482e2684510f39c07ce0dcf11f47f8
Ricky McConnell <mr.mcconnell12@yahoo.com>
2019-12-20 20:35:58 -0600

    Finished part 5 of select in select

--- 04_select_in_select.sql
+++ 04_select_in_select.sql
@@ -10,3 +10,26 @@ SELECT name FROM world
     AND gdp/population > (SELECT gdp/population
                             FROM world
                            WHERE name = 'United Kingdom');
+
+-- 3. List the name and continent of countries in the continents containing either Argentina or Australia. Order by name of the country.
+SELECT name, continent
+FROM world
+WHERE continent IN (SELECT continent
+                    FROM world
+                    WHERE name in ('Argentina', 'Australia'))
+    ORDER BY name;
+
+-- 4. Which country has a population that is more than Canada but less than Poland? Show the name and the population.
+SELECT name, population FROM world
+  WHERE population > (SELECT population FROM world
+                        WHERE name = 'Canada')
+    AND population < (SELECT population FROM world
+                        WHERE name = 'Poland');
+
+-- 5. Show the name and the population of each country in Europe. Show the population as a percentage of the population of Germany.
+SELECT name,
+    CONCAT(ROUND(100*population/ (SELECT population
+                                FROM world
+                                WHERE name = 'Germany')), '%')
+FROM world
+WHERE continent = 'Europe'
